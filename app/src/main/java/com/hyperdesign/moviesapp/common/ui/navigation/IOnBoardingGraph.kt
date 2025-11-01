d636e78329b9088428b35998c9d84fd497770102
Hyper Design <info@hyper-design.com>
2025-11-01 02:44:18 +0200

    Refactor: Rename OnBoardingDistenation to OnBoardingGraph
    The data object `OnBoardingDistenation` has been renamed to `OnBoardingGraph` to better reflect that it represents a navigation graph. The usage in the `buildNavBoardingGraph` extension function has been updated accordingly.
    
    Additionally, the unused `dialog` import from `androidx.navigation.compose.dialog` has been removed.

--- app/src/main/java/com/hyperdesign/moviesapp/common/ui/navigation/IOnBoardingGraph.kt
+++ app/src/main/java/com/hyperdesign/moviesapp/common/ui/navigation/IOnBoardingGraph.kt
@@ -2,21 +2,20 @@ package com.hyperdesign.moviesapp.common.ui.navigation
 
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.dialog
 import androidx.navigation.navigation
 import kotlinx.serialization.Serializable
 
 interface IOnBoardingGraph {
 
     @Serializable
-    data object OnBoardingDistenation : IGraph
+    data object OnBoardingGraph : IGraph
     @Serializable
     data object SplashScreenDestination : IDestination
 
 
 }
 fun NavGraphBuilder.buildNavBoardingGraph() {
-    navigation<IOnBoardingGraph.OnBoardingDistenation>(startDestination = IOnBoardingGraph.SplashScreenDestination) {
+    navigation<IOnBoardingGraph.OnBoardingGraph>(startDestination = IOnBoardingGraph.SplashScreenDestination) {
         composable<IOnBoardingGraph.SplashScreenDestination> {
 //            SplashScreen()
         }
