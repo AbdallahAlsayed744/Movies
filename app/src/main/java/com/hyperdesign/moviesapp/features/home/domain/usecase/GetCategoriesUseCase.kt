81f8f86f5cc10cda16d6be2d2355961a05a8f1ef
Hyper Design <info@hyper-design.com>
2025-12-21 21:48:16 +0200

    Refactor: Update GetCategoriesUseCase to depend on IHomeRepo interface
    The `GetCategoriesUseCase` has been updated to use the `IHomeRepo` interface instead of the concrete `HomeRepo` implementation. This change promotes better abstraction and facilitates easier testing by decoupling the use case from the specific repository implementation.

--- app/src/main/java/com/hyperdesign/moviesapp/features/home/domain/usecase/GetCategoriesUseCase.kt
+++ app/src/main/java/com/hyperdesign/moviesapp/features/home/domain/usecase/GetCategoriesUseCase.kt
@@ -2,12 +2,12 @@ package com.hyperdesign.moviesapp.features.home.domain.usecase
 
 import com.hyperdesign.moviesapp.common.domain.model.Resource
 import com.hyperdesign.moviesapp.common.domain.usecase.BaseUseCase
-import com.hyperdesign.moviesapp.features.home.data.repo.HomeRepo
 import com.hyperdesign.moviesapp.features.home.domain.model.CategoryResponse
+import com.hyperdesign.moviesapp.features.home.domain.repo.IHomeRepo
 import kotlinx.coroutines.flow.Flow
 
 class GetCategoriesUseCase(
-    private val homeRepo: HomeRepo
+    private val homeRepo: IHomeRepo
 ): BaseUseCase<Flow<Resource<CategoryResponse>>, Unit>() {
     override suspend fun invoke(body: Unit): Flow<Resource<CategoryResponse>> {
         return flowExecute {
